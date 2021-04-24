3c18235fca2921454b4b547e1d51ab78f26b0029
DongHunKim <ultraraptor@naver.com>
2021-04-24 21:39:00 +0900

    first commit

---
+++ src/test/java/com/example/SpringBootPlaygound/SpringBootPlaygoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootPlaygound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlaygoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
