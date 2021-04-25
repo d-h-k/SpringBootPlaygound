0155f9302f86147759502f401f38564df3a39293
DongHunKim <ultraraptor@naver.com>
2021-04-25 11:27:02 +0900

    테스트 코드 작성

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -2,10 +2,15 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE user(
     id int auto_increment primary key,
-    email varchar(64) unique ,
+    email varchar(64) unique not null,
     name varchar(64)
 );
 
 INSERT INTO user(email, name)
     values ('hinux@aho.com', 'honuz'), ('devdong@db.man', 'devdong');
 
+--explain select count(*) from user;
+--여기서만 안돌고 터미널이나 시퀄에서는 잘도는 쿼리
+
+
+-- 야 저거 위에 경고창 뜨는거 어캐없애냐 나중에 없애놔라
--- src/test/java/com/example/SpringBootPlaygound/SpringBootPlaygoundApplicationTests.java
+++ src/test/java/com/example/SpringBootPlaygound/SpringBootPlaygoundApplicationTests.java
@@ -45,8 +45,44 @@ class SpringBootPlaygoundApplicationTests {
 
 		Iterable<User> users = userRepo.findAll();
 		//assertThat(((Collection) user).size()).isEqualTo(2);
-        //이거뭐하는거지 왜 에러나는거지
+        //이거뭐하는거지 왜 에러나는거지 ->> 이거 그냥 몇개들어있는지 확인하는거임
         // 이게 콜렉션인지 확인하는거 필요해
+        // 이게 풀스캔이라는건데 안좋은거야
+
+
+        assertThat(userRepo.count()).isEqualTo(2);
+        //이건 풀스캔이 아니야 프라이머리키로 돌리는거야 삐트리의 인덱스를 검색하는거
+        //디비는 메타데이터가 없어
+
     }
+
+
+    @Test
+    @DisplayName("이름으로 이메일을 쿼리하고, 이메일로 이름을 조회하는 테스트")
+    void readUserNameAndEmail() {
+        User user = new User("test@test.com", "tester");
+        String wrongName = "devilCry";
+        String wrongEmail = "holy@moly.net";
+
+        userRepo.save(user);
+
+        User emailUser = userRepo.findByEmail(user.getEmail()).get();
+        assertThat(emailUser.getName()).isEqualTo(user.getName());
+
+        User nameUser = userRepo.findByName(user.getName()).get();
+        assertThat(nameUser.getEmail()).isEqualTo(user.getEmail());
+
+        assertThat(userRepo.findByName(wrongName).isPresent()).isFalse();
+        assertThat(userRepo.findByEmail(wrongEmail).isPresent()).isFalse();
+
+    }
+
+    /*
+    @Test
+    @DisplayName("새로운 유저 생성이 되는지 확인")
+    void create() {
+        User user = new User("test@zz.com", "hiho");
+        user.se
+    }*/
 
 }
