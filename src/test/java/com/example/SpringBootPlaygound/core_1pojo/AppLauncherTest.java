68a172d2df5709d31aae598ed6b1e2ec99dbf937
DongHunKim <dh.kim@bfactory.inc>
2021-07-27 14:00:57 +0900

    refactor : 테스트 작성

---
+++ src/test/java/com/example/SpringBootPlaygound/core_1pojo/AppLauncherTest.java
@@ -0,0 +1,5 @@
+package com.example.SpringBootPlaygound.core_1pojo;
+
+class AppLauncherTest {
+
+}
---
+++ src/test/java/com/example/SpringBootPlaygound/core_1pojo/order/discountpolicy/FixDiscountPolicyTest.java
@@ -0,0 +1,5 @@
+package com.example.SpringBootPlaygound.core_1pojo.order.discountpolicy;
+
+class FixDiscountPolicyTest {
+
+}
